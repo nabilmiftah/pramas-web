db386ef75c82dcf7e4cd0a6635c6e957b39e906b
miftah <miftahulnbl56@gmail.com>
2026-07-07 19:57:06 +0700

    Add file BookingService

---
+++ src/services/BookingService.js
@@ -0,0 +1,25 @@
+import BaseService from './BaseService.js';
+
+class BookingService extends BaseService {
+  constructor() {
+    // Mengirim nama tabel 'Booking_Transaksi' ke BaseService
+    super('Booking_Transaksi');
+  }
+
+  // Method KHUSUS untuk menambahkan data booking baru (Insert)
+  // Ini menunjukkan bahwa class anak bisa punya kemampuan spesifik
+  async createBooking(dataBooking) {
+    const { data, error } = await this.db
+      .from(this.tableName)
+      .insert([dataBooking])
+      .select(); // Mengembalikan data yang baru saja dimasukkan
+
+    if (error) {
+      console.error(`Error membuat booking baru:`, error.message);
+      throw new Error(error.message);
+    }
+    return data;
+  }
+}
+
+export default new BookingService();
