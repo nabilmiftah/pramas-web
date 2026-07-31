ee7d32261e949175ccb372353dd8b8f6a746f6e8
miftah <miftahulnbl56@gmail.com>
2026-07-31 07:48:24 +0700

    fix: update supabaseClient

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://dgfiqnvzhikdwjnkxctd.supabase.co';
-const supabaseKey = 'sb_publishable_2NwF-TweJxqmVW_D8WIL8w_PuYTCXlY';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
